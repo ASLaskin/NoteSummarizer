e4632c38c8673f7bde60e39d3a7eaaf07891902b
ASLaskin <andrewlaskin@ufl.edu>
2025-06-23 13:44:51 -0400

    added plugins for the editor

---
+++ app/editor/page.tsx
@@ -0,0 +1,9 @@
+import { Editor } from "@/components/blocks/editor-00/editor";
+
+export default function EditorPage() {
+  return (
+    <div className="container mx-auto p-4">
+      <Editor />
+    </div>
+  );
+}
